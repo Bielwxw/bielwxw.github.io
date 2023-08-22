a331fd3f4230986f22299246519502d4a3a6a3b9
Gabriel Rodrigues <gabrielrodriguescalixto7@gmail.com>
2023-08-22 10:22:29 -0300

    new code

--- Atividade6/script.js
+++ Atividade6/script.js
@@ -1,10 +1,9 @@
 const body = document.body;
 const themeButton = document.getElementById("alternar");
 
-// Verifique se há uma preferência de tema armazenada no localStorage
 const themePreference = localStorage.getItem("themePreference");
 
-// Se houver uma preferência de tema no localStorage, aplique-a
+
 if (themePreference) {
   body.classList.add(themePreference);
 }
@@ -12,9 +11,9 @@ if (themePreference) {
 themeButton.addEventListener("click", () => {
   if (body.classList.contains("dark-theme")) {
     body.classList.remove("dark-theme");
-    localStorage.setItem("themePreference", ""); // Limpe a preferência de tema no localStorage
+    localStorage.setItem("themePreference", ""); 
   } else {
     body.classList.add("dark-theme");
-    localStorage.setItem("themePreference", "dark-theme"); // Armazene a preferência de tema no localStorage
+    localStorage.setItem("themePreference", "dark-theme"); 
   }
 });
