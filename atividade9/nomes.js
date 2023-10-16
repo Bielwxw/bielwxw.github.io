88359f4e1b32475290affad25d7cefb365297c13
Gabriel Rodrigues <133263110+Bielwxw@users.noreply.github.com>
2023-10-16 14:16:08 -0300

    Update nomes.js

--- atividade9/nomes.js
+++ atividade9/nomes.js
@@ -3,7 +3,7 @@ const sobrenome = document.getElementById("sobrenome");
 const nome = document.getElementById("nome");
 
 function separar() {
-    const valor = nomeCompleto.value.trim().replace("  ", " ");
+    const valor = nomeCompleto.value.trim().replace(/ +/g, " ");
     let nomeSobrenome = valor.split(' ');
     nome.innerHTML = nomeSobrenome[0];
     sobrenome.innerHTML = nomeSobrenome[1] || '';
