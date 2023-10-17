148630402bbfae0f3dcd9eaf8888cde3f7c3feed
Gabriel Rodrigues <grc3@aluno.ifal.edu.br>
2023-10-17 07:11:49 -0300

    novo

--- atividade10/robotron2.js
+++ atividade10/robotron2.js
@@ -1,5 +1,33 @@
 const buttons = Array.from(document.getElementsByClassName("controle-ajuste"));
+const botaoAnterior = document.getElementById("anterior");
+const botaoProximo = document.getElementById("proximo");
 
+const listaRobotron = document.getElementById("robotron-container").children;
+
+// Troca de cor
+let lastIndex = listaRobotron.length - 1;
+let indexRobo = 0;
+botaoAnterior.addEventListener("click", () => {
+    esconder(indexRobo);
+    indexRobo = (indexRobo === 0) ? lastIndex : indexRobo - 1;
+    mostrar(indexRobo);
+});
+
+botaoProximo.addEventListener("click", () => {
+    esconder(indexRobo);
+    indexRobo = (indexRobo === lastIndex) ? 0 : indexRobo + 1;
+    mostrar(indexRobo);
+});
+
+function esconder(index) {
+    listaRobotron[index].style.display = "none";
+}
+
+function mostrar(index) {
+    listaRobotron[index].style.display = "block";
+}
+
+// Mudar Valores do Input
 buttons.forEach(botao => {
     botao.addEventListener("click", mudarValor);
 });
