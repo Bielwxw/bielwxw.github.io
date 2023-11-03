c282fd45e65ec03f0e9c35c619bdbbf159e909e5
Gabriel Rodrigues <grc3@aluno.ifal.edu.br>
2023-11-02 23:47:59 -0300

    atualizacao

--- Projeto-corretoras/script.js
+++ Projeto-corretoras/script.js
@@ -4,10 +4,8 @@ icon.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme');
     if (document.body.classList.contains('dark-theme')) {
         icon.innerHTML = "sunny";
-        localStorage.setItem('dark-theme', false);
     }
     else {
         icon.innerHTML = "dark_mode";
-        localStorage.setItem('dark-theme', true);
     }
 });
